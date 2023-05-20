3832ee29e113aa2364f9abfe5dd9939a050a024c
Imanullah <pengguna21@gmail.com>
2023-05-20 21:53:49 +0700

    fix | port

--- src/main.ts
+++ src/main.ts
@@ -46,6 +46,6 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
