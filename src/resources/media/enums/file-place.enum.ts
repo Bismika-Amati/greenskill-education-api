504cf2c9f64cba5fd49d1d5a74c7070352e41826
Imanullah <pengguna21@gmail.com>
2023-06-09 01:32:22 +0700

    add | user picture

--- src/resources/media/enums/file-place.enum.ts
+++ src/resources/media/enums/file-place.enum.ts
@@ -2,6 +2,7 @@ export const FilePlace = {
   VILLAGE_PICTURE: 'village-picture',
   COURSE_PICTURE: 'course-picture',
   SUBMODULE_PICTURE: 'submodule-picture',
+  USER_PICTURE: 'user-picture',
 };
 
 export type FilePlace = (typeof FilePlace)[keyof typeof FilePlace];
